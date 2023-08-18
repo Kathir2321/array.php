9f7bbbecc00149eca66b7322e2e8534d30575a65
Kathir2321 <142398049+Kathir2321@users.noreply.github.com>
2023-08-18 18:13:34 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Array</h1>
+    <?php
+    $Bikes = array("Fz","R15","Ninja","Tvs");
+    var_dump($Bikes);
+?>
+<br>
+<?php
+$Bikes = array("Fz","R15","Ninja","Tvs");
+echo $Bikes[0],"<br>";
+echo $Bikes[3];
+?>
+</body>
+</html>
